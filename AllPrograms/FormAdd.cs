63f0e7c5692e444d94ce2d470eec20e7be0bd28d
Vitaliy <mitradis@gmail.com>
2024-12-21 14:22:01 +0300

    Clear ReUpload

---
+++ AllPrograms/FormAdd.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace AllPrograms
+{
+    public partial class FormAdd : Form
+    {
+        bool setArgs = false;
+
+        public FormAdd(bool args)
+        {
+            InitializeComponent();
+            label1.Text = args ? "Ключ(и) запуска:" : "Название ярлыка:";
+            setArgs = args;
+        }
+
+        void button1_Click(object sender, System.EventArgs e)
+        {
+            if (setArgs)
+            {
+                FormMain.newAppArgs = textBox1.Text;
+            }
+            else
+            {
+                FormMain.newAppName = textBox1.Text;
+            }
+        }
+    }
+}
